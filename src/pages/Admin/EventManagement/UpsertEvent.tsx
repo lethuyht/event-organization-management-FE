bd606c6aba99594a64d7877ffb276c91264ff1a4
Cuong Tran <cuongtran11052000@gmail.com>
2023-07-02 22:53:49 +0700

    fix create event

--- src/pages/Admin/EventManagement/UpsertEvent.tsx
+++ src/pages/Admin/EventManagement/UpsertEvent.tsx
@@ -214,7 +214,7 @@ export function UpsertEvent() {
             required
           >
             <DynamicEventServiceItem
-              initialValues={data?.getEvent?.eventServiceItems as any}
+              initialValues={data?.getEvent?.eventServiceItems || []}
               form={form}
             />
           </Form.Item>
